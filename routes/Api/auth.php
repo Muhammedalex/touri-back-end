51b257919cf3c916b6d3cf6a8a63050d5ab376a4
Muhammedalex <146328429+Muhammedalex@users.noreply.github.com>
2024-02-28 13:17:36 +0200

    go

--- routes/Api/auth.php
+++ routes/Api/auth.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\EmailVerificationController;
 
-Route::post('/auth/register',[RegisterController::class , 'register']);
+Route::post('/auth/registerr',[RegisterController::class , 'register']);
 Route::post('/auth/login',[LoginController::class , 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/email', [EmailVerificationController::class, 'email']);
